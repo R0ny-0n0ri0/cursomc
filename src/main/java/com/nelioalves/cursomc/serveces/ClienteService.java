10011f9204a0833eeb442557931dfc1d8d38a56a
Rony Onorio <ronyonorio89@gmail.com>
2022-04-08 12:33:12 -0300

    Endpoint /cliente/{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/serveces/ClienteService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.serveces;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositores.ClienteRepository;
+import com.nelioalves.cursomc.serveces.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id)  { 
+		 Optional<Cliente> obj = repo.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException( 
+		  "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+		 } 
+}
