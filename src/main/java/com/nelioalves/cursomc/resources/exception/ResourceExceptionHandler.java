fc27bf7fae07d6374b4041ea7272655e4b61bdcb
Rony Onorio <ronyonorio89@gmail.com>
2022-04-19 10:57:41 -0300

    Restrição de conteúdo: Cliente só recupera ele mesmo

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.serveces.exceptions.AuthorizationException;
 import com.nelioalves.cursomc.serveces.exceptions.DateIntegrityException;
 import com.nelioalves.cursomc.serveces.exceptions.ObjectNotFoundException;
 
@@ -37,5 +38,12 @@ public class ResourceExceptionHandler {
 			err.addError(x.getField(), x.getDefaultMessage());
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
 	}
 }
